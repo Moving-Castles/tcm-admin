4e95f181af170f20d970bfc8efbd80c5b4a568f3
PWR <pwrpntr@gmail.com>
2024-04-09 08:50:31 +0200

    Remove unused schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,8 +1,6 @@
 import Loading from './Loading'
-import Spawning from './Spawning'
 import Tutorial from './Tutorial'
 import ContentEditor from "./ContentEditor"
-import Level from './Level'
 import Machine from './Machine'
 
-export const schemaTypes = [Loading, Spawning, Level, Machine, ContentEditor, Tutorial]
+export const schemaTypes = [Loading, Machine, ContentEditor, Tutorial]
